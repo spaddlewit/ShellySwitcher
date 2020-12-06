371263c848bfcf90660bb2676c701dea6021de33
spaddlewit <spaddlewit@gmail.com>
2020-12-06 17:38:21 -0500

    v1.0.0

--- Shelly Switcher WatchKit Extension/ComplicationController.m
+++ Shelly Switcher WatchKit Extension/ComplicationController.m
@@ -1,9 +1,18 @@
-//
-//  ComplicationController.m
-//  Shelly Switcher WatchKit Extension
-//
-//  Created by Arthur Freda on 11/30/20.
-//
+/*
+ Copyright 2020 Spaddlewit Inc. www.spaddlewit.com
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "ComplicationController.h"
 
@@ -43,19 +52,50 @@
 
 - (void)getCurrentTimelineEntryForComplication:(CLKComplication *)complication withHandler:(void(^)(CLKComplicationTimelineEntry * __nullable))handler {
     // Call the handler with the current timeline entry
-    handler(nil);
-}
+    if (complication.family == CLKComplicationFamilyModularSmall)
+    {
+        UIImage *iconImage = [UIImage imageNamed:@"SwitchOn"];
+        CLKComplicationTemplateModularSmallSimpleImage *clkSI = [[CLKComplicationTemplateModularSmallSimpleImage alloc] initWithImageProvider:[[CLKImageProvider alloc] initWithOnePieceImage:iconImage]];
 
+        CLKComplicationTimelineEntry *entry = [CLKComplicationTimelineEntry entryWithDate:[NSDate date] complicationTemplate:clkSI];
+        handler(entry);
+    }
+    else if (complication.family == CLKComplicationFamilyGraphicCircular)
+    {
+        UIImage *iconImage = [UIImage imageNamed:@"SwitchOn"];
+        CLKComplicationTemplateGraphicCircularImage *clkSI = [[CLKComplicationTemplateGraphicCircularImage alloc] initWithImageProvider:[[CLKFullColorImageProvider alloc] initWithFullColorImage: iconImage]];
+                                                              
+        CLKComplicationTimelineEntry *entry = [CLKComplicationTimelineEntry entryWithDate:[NSDate date] complicationTemplate:clkSI];
+        handler(entry);
+    }
+    else
+        handler(nil);
+}
+/*
 - (void)getTimelineEntriesForComplication:(CLKComplication *)complication afterDate:(NSDate *)date limit:(NSUInteger)limit withHandler:(void(^)(NSArray<CLKComplicationTimelineEntry *> * __nullable entries))handler {
     // Call the handler with the timeline entries after the given date
     handler(nil);
 }
-
+*/
 #pragma mark - Sample Templates
 
 - (void)getLocalizableSampleTemplateForComplication:(CLKComplication *)complication withHandler:(void(^)(CLKComplicationTemplate * __nullable complicationTemplate))handler {
     // This method will be called once per supported complication, and the results will be cached
-    handler(nil);
+    
+    if (complication.family == CLKComplicationFamilyModularSmall)
+    {
+        UIImage *iconImage = [UIImage imageNamed:@"SwitchOn"];
+        CLKComplicationTemplateModularSmallSimpleImage *clkSI = [[CLKComplicationTemplateModularSmallSimpleImage alloc] initWithImageProvider:[[CLKImageProvider alloc] initWithOnePieceImage:iconImage]];
+        handler(clkSI);
+    }
+    else if (complication.family == CLKComplicationFamilyGraphicCircular)
+    {
+        UIImage *iconImage = [UIImage imageNamed:@"SwitchOn"];
+        CLKComplicationTemplateGraphicCircularImage *clkSI = [[CLKComplicationTemplateGraphicCircularImage alloc] initWithImageProvider:[[CLKFullColorImageProvider alloc] initWithFullColorImage: iconImage]];
+        handler(clkSI);
+    }
+    else
+        handler(nil);
 }
 
 @end
--- Shelly Switcher WatchKit Extension/SettingsController.mm
+++ Shelly Switcher WatchKit Extension/SettingsController.mm
@@ -1,11 +1,22 @@
-//
-//  SettingsController.m
-//  Shelly Switcher WatchKit Extension
-//
-//  Created by Arthur Freda on 11/30/20.
-//
+/*
+ Copyright 2020 Spaddlewit Inc. www.spaddlewit.com
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "SettingsController.h"
+#import "DeviceRowController.h"
+#import "DataFileManager.hpp"
 
 @implementation SettingsController
 
@@ -19,6 +30,7 @@
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
     [super willActivate];
+    [self refreshList];
 }
 
 - (void)didDeactivate {
@@ -26,7 +38,54 @@
     [super didDeactivate];
 }
 
-- (IBAction)addDevice {
+- (void)refreshList {
+    
+    unsigned long rowCount = Listable::GetCount(shellyList);
+    [self.tblDevices setNumberOfRows:rowCount withRowType:@"deviceRowType2"];
+
+    rowCount = (unsigned int)self.tblDevices.numberOfRows;
+        
+    // Iterate over the rows and set the label and image for each one.
+    for (int i = 0; i < rowCount; i++) {
+        // Set the values for the row controller
+        DeviceRowController *row = (DeviceRowController*)[self.tblDevices rowControllerAtIndex:i];
+        
+        const ShellyItem *item = (ShellyItem*)Listable::GetByIndex(shellyList, i);
+        if (item)
+        {
+            [row configureWithItem:item atIndex:i];
+        }
+    }
+}
+
+- (void)table:(WKInterfaceTable *)table didSelectRowAtIndex:(NSInteger)rowIndex {
+    
+    ShellyItem *item = (ShellyItem*)Listable::GetByIndex(shellyList, (unsigned int)rowIndex);
+    
+    if (item == NULL)
+        return;
+    
+    currentShelly = item;
+    
+    // Ask 'Do you wish to remove GetName()?
+    NSString *prompt = [NSString stringWithFormat:@"Do you wish to remove %s?", item->GetName()];
+    [self presentAlertControllerWithTitle:nil message:prompt preferredStyle:WKAlertControllerStyleAlert actions:@[
+        [WKAlertAction actionWithTitle: @"Yes"
+                       style: WKAlertActionStyleDefault
+                       handler: ^{
+            //something after clicking OK
+            Listable::Remove(item, (Listable**)&shellyList);
+            currentShelly = NULL;
+            [self refreshList];
+                       }
+     ],
+     [WKAlertAction actionWithTitle: @"No"
+                    style: WKAlertActionStyleDefault
+                    handler: ^{
+         //something after clicking OK
+                    }
+      ]]
+     ];
 }
 
 @end
